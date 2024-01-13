4c2c8430a02c0838662da4d45750e4a52cdc45f4
hieu <nguyenxuanhieuktb10@gmail.com>
2024-01-13 17:03:55 +0700

    ICG-53: Update socket msg and render

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+const SocketMsgModel = require('./msg.model.js');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
@@ -6,78 +6,65 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server)
 
+let count = 0
+
 io.eio.pingTimeout = 120000;
 io.eio.pingInterval = 120000;
 
 rooms = new Map();
 
 app.use(express.json())
+
 io.on('connection', (socket) => {
-
-    socket.on('user_disconnect', (msg) => {
-        const id = msg.id;
-        console.log(`User ${id} disconnected`);
-        for(const key of rooms.keys()){
-            if(key.includes(id)){
-                socket.to(key).emit('user_disconnect', id);
-                rooms.delete(key);
-                socket.leave(key);
-            }
-        }
-    })
-
     console.log(`User ${socket.id} connected`);
-    
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
+    socket.emit('connected', { id: socket.id });
 
     socket.on('disconnect', () => {
         console.log(`User ${socket.id} disconnected`);
+        const socketId = socket.id;
+
+        for (let [key, value] of rooms) {
+            console.log(key, value);
+            if (key.includes(socketId)) {
+                rooms.delete(key);
+            }
+        }
     });
-    
+
     socket.on('request_ride', (msg) => {
-        console.log(`User ${socket.id} requested a ride`)
-        console.log(`msg: ${msg}`)
-        console.log(`id: ${msg.customerId}`)
-        io.emit('request_ride', {
-            customerId: msg.customerId,
-            id: socket.id,
-            position: msg.position,
-        });
+        const socketMsg = SocketMsgModel.fromJson(msg);
+        socketMsg.customerSocketId = socket.id;
+        io.emit('request_ride', socketMsg);
     })
 
     socket.on('accept_ride', (msg) => {
-        console.log('msg: ', msg);
-        const ids = [msg.customerId, msg.driverId].sort();
-        const idRoom = ids[0] + '###' + ids[1];
+        console.log('accept_ride', msg);
+        const socketMsg = SocketMsgModel.fromJson(msg);
+        socketMsg.driverSocketId = socket.id;
+        const ids = [socketMsg.customerId, socketMsg.driverId, socketMsg.customerSocketId, socketMsg.driverSocketId].sort();
+        const idRoom = ids.join('###');
         socket.join(idRoom);
 
-
-        console.log(`User ${socket.id} accepted a ride`)
-        console.log(`msg: ${msg.customerId}, ${msg.driverId}`)
-        console.log(`idRoom: ${idRoom}`)
-
-        // case of customer
-        if(!rooms.has(idRoom)){
-            rooms.set(idRoom, ids);
-            io.to(msg.customerSocketId).emit('accept_ride' ,msg);
+        if (!rooms.has(idRoom) || rooms.get(idRoom) == false) {
+            rooms.set(idRoom, true);
+            io.to(socketMsg.customerSocketId).emit('accept_ride', socketMsg);
         }
     })
 
     socket.on('send_location', (msg) => {
-        const ids = [msg.customerId, msg.driverId].sort();
-        const idRoom = ids[0] + '###' + ids[1];
-
-        console.log(`User ${socket.id} sent location`, msg);
+        const socketMsg = SocketMsgModel.fromJson(msg);
+        const ids = [socketMsg.customerId, socketMsg.driverId, socketMsg.customerSocketId, socketMsg.driverSocketId].sort();
+        const idRoom = ids.join('###');
         
-        if(!rooms.has(idRoom)){
+        console.log(`User ${socket.id} sent location`, msg);
+        console.log(`send ${count++}`);
+
+        if (!rooms.has(idRoom)) {
             console.log(`Room ${idRoom} not found`);
             return;
         }
 
-        socket.to(idRoom).emit('send_location', msg);
+        socket.to(socketMsg.customerSocketId).emit('send_location', msg);
     });
 });
 
